f77b253e24e0475118a3efe378f345272f21bd0a
developerJhonAlon <jhonalonjami@gmail.com>
2020-03-21 23:27:07 -0500

    folder docs2

--- CultivoGeografico-ejb/target/generated-sources/annotations/com/msr/cultivo/dto/AgricultorDTO_.java
+++ CultivoGeografico-ejb/target/generated-sources/annotations/com/msr/cultivo/dto/AgricultorDTO_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-03-08T04:00:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-03-21T23:16:17")
 @StaticMetamodel(AgricultorDTO.class)
 public class AgricultorDTO_ { 
 
